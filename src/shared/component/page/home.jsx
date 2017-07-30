c3728d39b9ec59b5598751e5970e1d0763021c5c
Eric So <me@eric.so>
2017-07-30 11:41:25 -0700

    refactor component to have more explicit style

--- src/shared/component/page/home.jsx
+++ src/shared/component/page/home.jsx
@@ -8,24 +8,34 @@ import { APP_NAME } from '../../config';
 import styles from './homeStyles';
 
 
-const HomePage = ({ classes }: { classes: Object }) =>
-  <div>
-    <Helmet
-      meta={[
-        { name: 'description', content: 'Hello App is an app to say hello' },
-        { property: 'og:title', content: APP_NAME },
-      ]}
-    />
+type HomePagePropsType = {
+  classes: Object,
+};
 
-    <h1>{APP_NAME}</h1>
 
-    <div className="jss-example">
-      <h3 className="jss-header">JSS</h3>
-      <p className={classes.hoverMe}>Hover me.</p>
-      <p className={classes.resizeMe}>Resize the window.</p>
-      <button className={classes.specialButton}>Composition</button>
+const HomePage = (props: HomePagePropsType) => {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Helmet
+        meta={[
+          { name: 'description', content: 'Hello App is an app to say hello' },
+          { property: 'og:title', content: APP_NAME },
+        ]}
+      />
+
+      <h1>{APP_NAME}</h1>
+
+      <div className="jss-example">
+        <h3 className="jss-header">JSS</h3>
+        <p className={classes.hoverMe}>Hover me.</p>
+        <p className={classes.resizeMe}>Resize the window.</p>
+        <button className={classes.specialButton}>Composition</button>
+      </div>
+
     </div>
-
-  </div>;
+  );
+};
 
 export default injectSheet(styles)(HomePage);
